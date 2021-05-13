1720eea5239bf53b493cc0df66454c314676c224
keremcanb <keremcan@gmail.com>
2021-05-14 00:23:25 +0300

    m

--- next.config.js
+++ next.config.js
@@ -4,5 +4,13 @@ module.exports = {
   },
   images: {
     domains: ['res.cloudinary.com']
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'https://next-events.herokuapp.com/:path*'
+      }
+    ];
   }
 };
