b70468244dac85d598277174af7d6d8c769e8377
keremcanb <keremcan@gmail.com>
2021-05-22 00:06:28 +0300

    m

--- components/Search.js
+++ components/Search.js
@@ -20,7 +20,7 @@ const Search = () => {
     <form className="relative mt-1" onSubmit={handleSubmit}>
       <input
         type="text"
-        id="password"
+        id="search"
         value={term}
         onChange={handleChange}
         className="w-full pl-3 pr-10 py-2 border-2 border-gray-200 rounded-xl hover:border-gray-300 focus:outline-none focus:border-blue-500 transition-colors"
